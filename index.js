6f98ff26f7a892cd39588424401c7d79be2e0c98
Siebe Van Dijck <siebe.vandijck@codegent.com>
2015-05-28 11:56:08 +0100

    first update to gm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs     = require('fs');
 var xml2js = require('xml2js');
-var ig     = require('imagemagick');
+var gm     = require('gm');
 var colors = require('colors');
 var _      = require('underscore');
 var Q      = require('q');
@@ -114,21 +114,19 @@ var getProjectName = function () {
  */
 var generateIcon = function (platform, icon) {
     var deferred = Q.defer();
-    ig.resize({
-        srcPath: settings.ICON_FILE,
-        dstPath: platform.iconsPath + icon.name,
-        quality: 1,
-        format: 'png',
-        width: icon.size,
-        height: icon.size,
-    } , function(err, stdout, stderr){
-        if (err) {
-            deferred.reject(err);
-        } else {
-            deferred.resolve();
-            display.success(icon.name + ' created');
-        }
-    });
+    gm(settings.ICON_FILE)
+        .resize(width, height)
+        .gravity('center')
+        .write(file, function(err) {
+          
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve();
+                display.success(splash.name + ' created');
+            }
+        });
+
     return deferred.promise;
 };
 
@@ -236,16 +234,23 @@ var configFileExists = function () {
 
 display.header('Checking Project & Icon');
 
-atLeastOnePlatformFound()
-    .then(validIconExists)
-    .then(configFileExists)
-    .then(getProjectName)
-    .then(getPlatforms)
-    .then(generateIcons)
-    .catch(function (err) {
-        if (err) {
-            console.log(err);
-        }
-    }).then(function () {
-        console.log('');
-    });
+var run = function(){
+    atLeastOnePlatformFound()
+        .then(validIconExists)
+        .then(configFileExists)
+        .then(getProjectName)
+        .then(getPlatforms)
+        .then(generateIcons)
+        .catch(function (err) {
+            if (err) {
+                console.log(err);
+            }
+        }).then(function () {
+            console.log('');
+        });
+}
+
+module.exports = {
+  generate: run
+};
+
